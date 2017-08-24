9a11bc96669a43ae727bc46ed4096f2d2eede532
Chiel Bruin <chielbruin@tele2.nl>
2017-08-24 19:36:59 +0200

    Added the new template loader

---
+++ texvoiceTemplateLoader.py
@@ -0,0 +1,43 @@
+# coding: UTF-8
+
+import os, shutil
+from zipfile import ZipFile
+
+class TexvoiceTemplateLoader:
+	tmpFolder = '.tmp'
+	
+	@classmethod
+	def loadTemplate(cls, template):
+		cls.unpack(template)
+		
+		(res, templateVersion) = cls.validate()
+		if res == True:
+			return (cls.readTemplateFile(), templateVersion)
+		else:
+			cls.cleanup()
+			raise res
+
+	@classmethod
+	def validate():
+		# TODO check if all files exist and the template version
+		# return Exception('Invalid template file')
+		return (True, 0)
+		
+	@classmethod
+	def cleanup():
+		shutil.rmtree('.tmp')
+	
+	@classmethod
+	def unpack(cls, templateZip):
+		with ZipFile(templateZip, 'r') as archive:
+			archive.extractall(cls.tmpFolder)
+	
+	@classmethod
+	def readTemplateFile(cls):
+		content = ''	
+		with open(cls.tmpFolder + '/template.tex' , 'r') as infile:
+			line = infile.readline()
+			while line:
+				content += line
+				line = infile.readline()
+		return content
