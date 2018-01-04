4de08ab749a125f222df5b546ed3d0e94f22a4a1
Chiel Bruin <github@bruin.ch>
2018-01-04 14:48:15 +0100

    Many fixes, stof now kinda works

--- src/tvTemplate.py
+++ src/tvTemplate.py
@@ -17,65 +17,52 @@ class Template:
 		return self._tex
 
 	def applyListings(self, data):
-		listings = self._getListings()
-		result = ''
-		for (start, end) in listings:
+		while True:
+			(start, end) = self._findSection('texvoiceListing')
+			if start[0] is -1:
+				break
 			data = self._applyListing(start, end, data)
 			
 	def applyGlobals(self, data):
 		pass
 		
-	def _findSection(self, name, beg=0):
+	def _findSection(self, name, beg=0, tex=None):
 		'''
 		Find a section with a given name in the input.
 		When a starting point is given only returns a section after that point
-		'''		
+		'''
+		if tex is None:
+			tex = self.tex
 		startStr = '\\begin{%s}' % name
 		endStr = '\\end{%s}' % name
 		
-		start = self.tex.find(startStr, beg)
+		start = tex.find(startStr, beg)
 		if start is -1:
 			return ((-1, len(startStr)), (-1, len(endStr)))
-		end = self._tex.find(endStr, start)
+		end = tex.find(endStr, start)
 		if end is -1:
 			raise Exception('Cannot find closing tag for %s' % name)
 		return ((start, len(startStr)), (end, len(endStr)))
-		
-	def _getListings(self):
-		'''
-		Get a list of the positions of all the listings in the input
-		'''
-		res = []
-		finger = 0
-		self.tex	# Load if not present
-		while (True):
-			(start, end) = self._findSection('texvoiceListing', finger)
-			if (start[0] is -1):
-				return res
-			else:
-				finger = end[0] + end[1]
-				res.append((start, end))
 
 	def _applyListing(self, start, end, data):
 		'''
 		Apply as much of the data as is possible in the given range.
 		Returns the result and the data that couldn't be applied.
 		'''
-		
-		template = self.tex[ (start[0] + start[1]) : (end[0]) ]
 		res = ''
 		
 		while True:
-			(didApplyGroup, tmp) = (False, template)
-			
+			(didApplyGroup, tmp) = (False, self.tex[ (start[0] + start[1]) : (end[0]) ])
 			# For each group
 			for group in data:
-				didApplyGroup = self._applyGroup(data[group], group, start, end)
+				(didApply, tmp) = self._applyGroup(data[group], group, tex=tmp)
+				didApplyGroup = didApplyGroup or didApply
 			
 			# Global accumulates
-			didApplyGlobal = self._applyGlobal(data['global'], start, end)
+			didApplyGlobal = False
+			#(didApplyGlobal, tmp) = self._applyGlobal(data['global'], tex=tmp)
 			
-			# If you did apply anythng store it otherwise we are done 
+			# If you did apply anythng store it otherwise we are done
 			if didApplyGlobal or didApplyGroup:
 				 res += tmp
 			else:
@@ -83,20 +70,33 @@ class Template:
 		
 		self._tex = self._tex[:start[0]] + res + self._tex[end[0] + end[1]:]
 		return data
-
-	def _applyGroup(self, data, group, start, end, template=None):
+		
+	def applyField(self, key, value, tex):
+		return tex.replace('\\' + key, value)
+		
+	def _applyGroup(self, data, group, tex=None):
 		'''
 		Apply a single entry for the given group in the template.
 		Return True when an entry is applied, False otherwise.
 		'''
-		if template is None:
-			template = self.tex
-		#template = tex[start[0] + start[1]:end[0]]
-		#res = template
+		if tex is None:
+			tex = self.tex
+		(start, end) = self._findSection(group, tex=tex)
+		if start[0] is -1:
+			return (False, tex)
+			
+		if len(data['data']) is 0:
+			return (False, tex[:start[0]] + tex[end[0]+end[1]:])
+			
+		result = tex[start[0]+start[1]:end[0]]
+		row = data['data'].pop(0)
+		for index, key in enumerate(data['keys']):
+			value = row[index]
+			result = self.applyField(key, str(value), result)
 		
-		return False
+		return (True, tex[:start[0]] + result + tex[end[0]+end[1]:])
 
-	def _applyGlobal(self, data, start, end, template=None):
+	def applyGlobal(self, data, template=None):
 		'''
 		Apply a the global data in the template.
 		Return True when data is applied, False otherwise.
