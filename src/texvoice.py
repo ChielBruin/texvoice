0c6206fb1a03f8e49e795c45c4acca445d626577
Chiel Bruin <github@bruin.ch>
2018-01-12 18:15:11 +0100

    Outlined CLI functionalities

--- src/texvoice.py
+++ src/texvoice.py
@@ -3,39 +3,26 @@ import argparse
 import sys
 
 
-def getOptions(args):
-	'''
-	Get the options from the passed arguments
-	'''
-	options = { }
-
-	#TODO: remove these overrides
-	args.output = 'result.pdf'
-	args.input = 'timesheet2.csv'
-	
-	
-	options['keepSource'] = ('Keep the compiled text sources', args.keepSource)
-	options['resultFile'] = ('The file location where to store the result', args.output)
-	options['inputFile']  = ('The file location where to load the data from', args.input)
-	options['template']   = ('The template to use when compiling', 'templateTest.zip')
-
-	return options
-
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-m', '--mode', choices=['compile', 'load'],
-		help='Modes of operation, none for normal operation (GUI)')
+	# TODO use subparsers:
+	# texvoice [compile | loadData | full], where
+	# compile:  <inputFile, outputFile, template> [keepSource]
+	# loadData: <inputFile, outputFile> [loaderOptions]
+	# full:     <inputFile, outputFile, template> [loaderOptions, keepsource]
+	# Input and output file should be stdin and stdout by default
 	
-	parser.add_argument('-k', '--keepSource', type=bool, default=False,
-		help='Whether to keep the source files')
-	parser.add_argument('-i', '--input', type=argparse.FileType('r'), default=sys.stdin,
-		help='The input to process, stdin by default')
-	parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout,
-		help='The location to store the result, stdout by default')
+	#parser = argparse.ArgumentParser()
+	#parser.add_argument('-m', '--mode', choices=['compile', 'load'],
+		#help='Modes of operation, none for normal operation (GUI)')
+	
+	#parser.add_argument('-k', '--keepSource', type=bool, default=False,
+		#help='Whether to keep the source files')
+	#parser.add_argument('-i', '--input', type=argparse.FileType('r'), default=sys.stdin,
+		#help='The input to process, stdin by default')
+	#parser.add_argument('-o', '--output', type=argparse.FileType('w'), default=sys.stdout,
+		#help='The location to store the result, stdout by default')
 
-	args = parser.parse_args()
-	
-	options = getOptions(args)
+	#args = parser.parse_args()
 	
 	if args.mode:
 		if args.mode is 'compile':
